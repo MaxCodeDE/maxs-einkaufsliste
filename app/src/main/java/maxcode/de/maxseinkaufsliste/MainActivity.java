ba45396cb76ad2444f25e1e4bc648d68a03dd41d
Max <maxbremer@live.de>
2017-09-04 21:27:42 +0200

    delete selected action impl. + remove dialog

--- app/src/main/java/maxcode/de/maxseinkaufsliste/MainActivity.java
+++ app/src/main/java/maxcode/de/maxseinkaufsliste/MainActivity.java
@@ -3,6 +3,7 @@ package maxcode.de.maxseinkaufsliste;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -49,15 +50,11 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // Makiertes Item
                 String selectedItem = ((TextView) view).getText().toString();
-                if (selectedItems.contains(selectedItem)) {
-                    // Item entefenen
-                    selectedItems.remove(selectedItem);
+                if (itemIsSelected(selectedItem)) {
                     // Änderungen speichern
                     setItem(selectedItem, false);
                     loadItems();
                 } else {
-                    // Item zu makierten Items hinzufügen
-                    selectedItems.add(selectedItem);
                     // Änderungen speichern
                     setItem(selectedItem, true);
                     loadItems();
@@ -73,16 +70,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String newItem = editText.getText().toString();
                 if (newItem.equalsIgnoreCase("") || newItem == null) {
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setTitle("Fehler!")
-                            .setMessage("Das Eingabefeld ist leer.")
-                            .setCancelable(false)
-                            .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.cancel();
-                                }
-                            }).show();
+                    AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                    alert.setTitle("Fehler!");
+                    alert.setMessage("Das Eingabefeld ist leer.");
+                    alert.setCancelable(false);
+                    alert.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    alert.create().show();
                 } else {
                     // Eingabefeld leeren
                     editText.getText().clear();
@@ -112,25 +110,62 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
-            case R.id.deleteAllItemsButton:
-                // Items löschen
-                items.clear();
-                selectedItems.clear();
-                // Eingabefeld leeren
-                editText.getText().clear();
-                // Items aus DB löschen
-                SharedPreferences settings = getApplicationContext().getSharedPreferences(PREFS_NAME, getApplicationContext().MODE_PRIVATE);
-                settings.edit().clear().commit();
-                // Adapter neu laden -> Änderungen werden angezeigt
+            case R.id.deleteAllSelectedItemsButton:
+                for (int i = 0; i < items.size(); i++) {
+                    // GEspeicherte Items laden und in Array Speichern
+                    SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+                    Map<String, ?> allEntries = settings.getAll();
+                    for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
+                        if (Boolean.valueOf(entry.getValue().toString())) {
+                            SharedPreferences.Editor editor = settings.edit();
+                            editor.remove(entry.getKey());
+                            editor.apply();
+                        }
+                    }
+                }
+                loadItems();
                 arrayAdapter.notifyDataSetChanged();
-                // Info ausgeben
-                Toast toast = Toast.makeText(getApplicationContext(), "Liste geleert!", Toast.LENGTH_LONG);
-                toast.show();
+                //Toast toast = Toast.makeText(getApplicationContext(), "Alle makierten gelöscht!", Toast.LENGTH_LONG);
+                //toast.show();
+                return true;
+            case R.id.deleteAllItemsButton:
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Alle löschen?");
+                alert.setMessage("Sicher das alle Einträge gelöscht werden sollen?");
+                alert.setCancelable(true);
+                alert.setPositiveButton("Ja", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Items löschen
+                        items.clear();
+                        selectedItems.clear();
+                        // Eingabefeld leeren
+                        editText.getText().clear();
+                        // Items aus DB löschen
+                        SharedPreferences settings = getApplicationContext().getSharedPreferences(PREFS_NAME, getApplicationContext().MODE_PRIVATE);
+                        settings.edit().clear().commit();
+                        // Adapter neu laden -> Änderungen werden angezeigt
+                        arrayAdapter.notifyDataSetChanged();
+                        // Info ausgeben
+                        Toast toast = Toast.makeText(getApplicationContext(), "Liste geleert!", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                });
+
+                alert.setNegativeButton( "Nein", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+                AlertDialog alert11 = alert.create();
+                alert11.show();
                 return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
 
     public void setItem(String key, Boolean value) {
         // Änderungen speichern
@@ -138,6 +173,17 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = settings.edit();
         editor.putString(key, value.toString());
         editor.commit();
+    }
+
+    public Boolean itemIsSelected(String key) {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        Map<String, ?> allEntries = settings.getAll();
+        for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
+            if (key.equalsIgnoreCase(entry.getKey())) {
+                return Boolean.valueOf(entry.getValue().toString());
+            }
+        }
+        return false;
     }
 
     public void loadItems() {
